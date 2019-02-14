d4f09a7828b00106c9b59e4b0b70c65db81d53af
Pravallika Rikhala <rpraval1@binghamton.edu>
2019-02-14 00:38:48 -0500

    Added Restaurant Model

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,14 @@
-/* _app.js */
-import React from "react";
+/* /pages/_app.js */
+
+import Layout from "../components/Layout";
+import withData from "../lib/apollo";
+
 import App, { Container } from "next/app";
-import Head from "next/head";
+import React from "react";
 
-
-export default class MyApp extends App {
+class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
-
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx);
     }
@@ -15,22 +16,35 @@ export default class MyApp extends App {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, isAuthenticated, ctx } = this.props;
     return (
-      <>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-            crossOrigin="anonymous"
-          />
-        </Head>
-
-        <Container>
+      <Container>
+        <Layout isAuthenticated={isAuthenticated} {...pageProps}>
           <Component {...pageProps} />
-        </Container>
-      </>
+        </Layout>
+
+        <style jsx global>
+          {`
+            a {
+              color: white !important;
+            }
+            a:link {
+              text-decoration: none !important;
+              color: white !important;
+            }
+            a:hover {
+              color: white;
+            }
+            .card {
+              display: inline-block !important;
+            }
+            .card-columns {
+              column-count: 3;
+            }
+          `}
+        </style>
+      </Container>
     );
   }
 }
+export default withData(MyApp);
